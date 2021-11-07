f4dfdca2be35ca3351e9038d5c836a39f8a40993
Anton Surkis <antonsurkis@gmail.com>
2021-11-07 21:56:31 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$page = $_GET['page'];
+if (!$page) $page = 1;
+if ($page < 1) $page = 1;
+if ($page > 9) $page = 9;
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="stylesheet.css">
+  <script src="script.js"></script>
+</head>
+
+<body>
+  <a <?php
+  if ($page <= 1) {
+    echo 'style="visibility: hidden"';
+  } else {
+    printf('href="?page=%d"', $page - 1);
+  } ?> >&lt;&lt; Previous</a>
+  <a <?php
+  if ($page >= 9) {
+    echo 'style="visibility: hidden"';
+  } else {
+    printf('href="?page=%d"', $page + 1);
+  } ?> >Next &gt;&gt;</a>
+  <?php include sprintf('pages/page%02d.html', $page); ?>
+</body>
+
+</html>
